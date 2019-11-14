9da8122bdb62510d85fa6d951baddfe9d27c1658
Timon Post <timonpost@hotmail.nl>
2019-11-14 13:18:14 +0100

    - Database - Wpf - Config

---
+++ RoeiJeRot.Database/Database/RoeiJeRotDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace RoeiJeRot.Database.Database
+{
+    public class RoeiJeRotDbContext : DbContext
+    {
+        public RoeiJeRotDbContext()
+        {
+        }
+
+        public RoeiJeRotDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Boat> Boats { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{Environment.MachineName}.json", true)
+                .Build();
+            
+            optionsBuilder.UseSqlServer(configuration["connectionString"]);
+        }
+    }
+}
---
+++ RoeiJeRot.Database/Database/User.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoeiJeRot.Database.Database
+{
+    public class User 
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
