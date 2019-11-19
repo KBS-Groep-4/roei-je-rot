ef6ae8f3ad70e45aba56a36fe03dce81367570de
MrWaps <olafwapsenkamp@gmail.com>
2019-11-19 14:08:11 +0100

    Create PasswordHasher

---
+++ RoeiJeRot.Logic/PasswordHasher.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace RoeiJeRot.Logic
+{
+    class PasswordHasher
+    {
+        public bool compareHash(string dbHash, string password)
+        {
+            bool value = false;
+
+            if (dbHash == getPasswordHash(password))
+            {
+                value = true;
+            }
+
+            return value;
+        }
+
+        public string getPasswordHash(string password)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (byte b in crypto)
+            {
+                hash.Append(b.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+    }
+}
