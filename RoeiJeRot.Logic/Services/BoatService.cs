41a27177bce7b9f76122a71fb4e0e32e9fefff82
Ayrton-Taede Tromp <ayrton.taede@gmail.com>
2019-11-21 13:26:42 +0100

    Begin work on SailboatService

---
+++ RoeiJeRot.Logic/Services/BoatService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RoeiJeRot.Database.Database;
+using System.Linq;
+
+namespace RoeiJeRot.Logic.Services
+{
+    public interface IBoatService
+    {
+        List<SailingBoat> GetAllBoats();
+    }
+    class BoatService : IBoatService
+    {
+        private readonly RoeiJeRotDbContext _context;
+        public BoatService(RoeiJeRotDbContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Gives all the boats no matter of status
+        /// </summary>
+        /// <returns>All boats</returns>
+        public List<SailingBoat> GetAllBoats()
+        {
+            return _context.SailingBoats.ToList();
+        }
+
+        /// <summary>
+        /// Get all sailboats with by given typeId regardless of status
+        /// </summary>
+        /// <param name="typeId"></param>
+        /// <returns>Returns all boats of given typeId</returns>
+        public List<SailingBoat> GetAllBoats(int typeId)
+        {
+            return GetAllBoats().Where(boat => boat.typeId).ToList();
+        }
+    }
+}
---
+++ RoeiJeRot.Logic/Services/ReservationService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RoeiJeRot.Database.Database;
+using System.Linq;
+
+namespace RoeiJeRot.Logic.Services
+{
+    public interface IReservationService
+    {
+        bool PlaceReservation(int boatType, int memberId, DateTime reservationDate, TimeSpan duration);
+        void CancelBoatReservation(int reservationId);
+    }
+
+    class ReservationService : IReservationService
+    {
+        private readonly RoeiJeRotDbContext _context;
+
+        public ReservationService(RoeiJeRotDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Plaatst een reservatie voor een boot
+        /// </summary>
+        /// <param name="boatType">Type van de boot die je wilt reserveren</param>
+        /// <param name="memberid">Id van het lid</param>
+        /// <param name="reservationDate">Wanneer is de boot gereserveerd</param>
+        /// <param name="duration">Hoelang duurt deze reservatie</param>
+        /// <returns>Wanneer een reservatie is geplaatst true, als een reservatie niet geplaatst kan worden false</returns>
+        public bool PlaceReservation(int boatType, int memberid, DateTime reservationDate, TimeSpan duration)
+        {
+            return false;
+        }
+
+        public List<SailingBoat> GetAvailableBoats(DateTime reservationDate, TimeSpan duration)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Get all sailingReservations
+        /// </summary>
+        /// <returns>All sailingReservations</returns>
+        public List<SailingReservation> GetReservations()
+        {
+            return _context.Reservations.ToList();
+        }
+
+        public void CancelBoatReservation(int reservationId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
