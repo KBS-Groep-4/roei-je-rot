46c65b75ebcb79bce01968683c93d97139fb7c7b
MrWaps <olafwapsenkamp@gmail.com>
2019-11-21 13:18:38 +0100

    Login Unit Test

---
+++ RoeiJeRot.Test/LoginTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RoeiJeRot.Database.Database;
+using RoeiJeRot.Logic;
+using RoeiJeRot.Logic.Services;
+
+namespace RoeiJeRot.Test
+{
+    class TestUserService : IUserService
+    {
+        public List<User> GetUsers()
+        {
+            List<User> list = new List<User>();
+            list.Add(new User()
+            {
+                FirstName = "Paul",
+                LastName = "Hiemstra",
+                City = "Zwolle",
+                Country = "Nederland",
+                HouseNumber = "2",
+                Password = Hasher.Hash("abc"),
+                Username = "abc",
+                SailingLevel = 1,
+                StreetName = "Duckweg"
+            });
+
+            return list;
+        }
+    }
+    class LoginTests
+    {
+        [TestCase("abc", "abc")]
+        public void LoginUserPaul(string username, string password)
+        {
+            //Arrange
+            LoginLogic logic = new LoginLogic(new TestUserService());
+
+            //Act
+            bool value = logic.AuthenticateUser(username, password);
+
+            //Assert
+            Assert.AreEqual(value, true);
+        }
+    }
+}
