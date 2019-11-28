f78fd0fcb7e758caecfa090f7d20e19e1b46ac66
BuildTools <ayrton.taede@gmail.com>
2019-11-28 14:27:38 +0100

    List gets updated when input changes

--- RoeiJeRot.View.Wpf/ReserveerScherm.xaml.cs
+++ RoeiJeRot.View.Wpf/ReserveerScherm.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
 using RoeiJeRot.Database.Database;
 using RoeiJeRot.Logic.Services;
@@ -53,53 +54,77 @@ namespace RoeiJeRot.View.Wpf
 
                     Object selectedItemObject = AvailableBoats.SelectedItem;
                     if (selectedItemObject == null)
+                    {
+                        MessageBox.Show("Geen boot geselecteerd :(");
                         return;
+                    }
 
                     BoatTypeViewModel selectedType = (BoatTypeViewModel)selectedItemObject;
 
                     if (When.SelectedDate.HasValue)
                     {
                         reservationService.PlaceReservation(selectedType.Id, 3, When.SelectedDate.Value + time, duration);
+                        MessageBox.Show("Reservering geplaatst :D");
+                        UpdateAvailableList();
                     }
                 }
             }
         }
 
-        public ReserveerScherm()
+        public void OnReservationDetailChange(Object sender, EventArgs args)
         {
-            InitializeComponent();
+            UpdateAvailableList();
+        }
 
-            When.SelectedDate = DateTime.Today;
+        public void UpdateAvailableList()
+        {
+            TimeSpan time;
+            int durationInt;
+            if (TimeSpan.TryParse(Time.Text, out time) && Duration != null && int.TryParse(Duration.Text, out durationInt) &&
+                When.SelectedDate.HasValue)
+            {
+                List<SailingBoat> boats = reservationService.GetAvailableBoats(When.SelectedDate.Value + time,
+                    TimeSpan.FromMinutes(durationInt));
+                List<BoatType> availableTypes = new List<BoatType>();
 
+                foreach (var boat in boats)
+                {
+                    bool alreadyIn = false;
+                    foreach (var type in availableTypes)
+                    {
+                        if (type.Id == boat.BoatTypeId)
+                            alreadyIn = true;
+                    }
+
+                    if (!alreadyIn) availableTypes.Add(boat.BoatType);
+                }
+
+                ObservableAvailableTypes = new ObservableCollection<BoatTypeViewModel>(availableTypes
+                    .Select(type => new BoatTypeViewModel()
+                    {
+                        Id = type.Id,
+                        Name = type.Name,
+                        PossiblePassengers = type.PossiblePassengers,
+                        RequiredLevel = type.RequiredLevel
+                    })
+                    .ToList());
+
+                AvailableBoats.ItemsSource = ObservableAvailableTypes;
+            }
+            else if(AvailableBoats != null) AvailableBoats.ItemsSource = null;
+        }
+
+        public ReserveerScherm()
+        { 
             context = new RoeiJeRotDbContext();
             boatService = new BoatService(context);
             reservationService = new ReservationService(context, boatService);
 
-            List<SailingBoat> boats = reservationService.GetAvailableBoats(When.SelectedDate.Value, TimeSpan.FromHours(2));
-            List<BoatType> availableTypes = new List<BoatType>();
+            InitializeComponent();
 
-            foreach (var boat in boats)
-            {
-                bool alreadyIn = false;
-                foreach (var type in availableTypes)
-                {
-                    if (type.Id == boat.BoatTypeId)
-                        alreadyIn = true;
-                }
-                if(!alreadyIn) availableTypes.Add(boat.BoatType);
-            }
+            When.SelectedDate = DateTime.Today;
 
-            ObservableAvailableTypes = new ObservableCollection<BoatTypeViewModel>(availableTypes
-                .Select(type => new BoatTypeViewModel()
-                {
-                    Id = type.Id,
-                    Name = type.Name,
-                    PossiblePassengers = type.PossiblePassengers,
-                    RequiredLevel = type.RequiredLevel
-                })
-                .ToList());
-
-            AvailableBoats.ItemsSource = ObservableAvailableTypes;
+            UpdateAvailableList();
         }
     }
 }
